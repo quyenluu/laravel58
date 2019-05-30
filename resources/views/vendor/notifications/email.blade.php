f5f333a79be093764e9c96610fbb2744dca87f45
quyenlc <quyenlc@vietcas.vn>
2019-05-30 18:08:31 +0700

    update register mail template

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,6 @@
+@component('mail::message')
+<div style="background-color: beige">
+    <h1>Hello!</h1>
+    <p>Please click the button below to verify your email address.</p>
+</div>
+@endcomponent
